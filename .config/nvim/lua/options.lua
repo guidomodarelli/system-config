6ef7df0ca67f6b9969aaff726933ade94b71a16d
guidomodarelli25 <guidomodarelli25@gmail.com>
2022-09-02 00:29:44 -0300

    sort options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,63 +1,64 @@
 local options = {
-  breakindent = true,
-  backup = false,
-  backupskip = '/tmp/*',
-  title = true,
-  filetype = 'on',
-  modelines = 0,
-  formatoptions = 'tcqrn1',
-  textwidth = 79,
-  shiftround = false,
-  backspace = 'indent,eol,start',
-  ttyfast = true,
-  showmode = true,
-
-  encoding = 'utf-8',
-  fileencoding = 'utf-8',
-  ignorecase = true,
-  smartcase = true,
-
-  autoindent = true, -- Start new lines correctly indented
-  cursorline = true, -- Highlight the line where the cursor is (see cursorlineopt)
-  cursorlineopt = "number", -- Highlight the cursor line number (see cursorline)
-  winblend = 0,
-  wildoptions = "pum",
-  pumblend = 5,
-  background = "dark",
-  completeopt = { "menuone", "noselect" }, -- Completion engine options
-  fillchars = vim.opt.fillchars + "diff:╱", -- Interface styling (see listchars)
-  fixeol = true, -- Restore EOL at EOF if missing when writing
-  foldmethod = "marker", -- Only allow foldings with triple brackets
-  -- guicursor = { a = "block" }, -- Force cursor to be a block at all times
-  hidden = true, -- Hide inactive buffers instead of deleting them
-  hlsearch = true, -- Highlight all search matches
-  inccommand = "split", -- Incrementally show effects of commands, opens split
-  incsearch = true, -- Highlight search matches while writing (with hlsearch)
-  laststatus = 2, -- Use a global statusline instead of one per window
-  linebreak = true, -- Respect WORDS when wrap-breaking lines (see wrap)
-  mouse = "nvi", -- Allow mouse everywhere except in command line mode
-  expandtab = true, -- Do not expand tabs to spaces (see softtabstop)
-  wrap = false, -- Do not wrap text that reaches the window's width
-  number = true, -- Number column to the left (used with relativenumber)
-  nrformats = "unsigned", -- Treat all numbers as unsigned with <C-A> and <C-X>
+  autoindent     = true, -- Start new lines correctly indented
+  background     = "dark",
+  backspace      = 'indent,eol,start',
+  backup         = false,
+  backupskip     = '/tmp/*',
+  breakindent    = true,
+  cmdheight      = 1,
+  completeopt    = { "menuone", "noselect" }, -- Completion engine options
+  cursorline     = true, -- Highlight the line where the cursor is (see cursorlineopt)
+  cursorlineopt  = "number", -- Highlight the cursor line number (see cursorline)
+  encoding       = 'utf-8',
+  expandtab      = true, -- Do not expand tabs to spaces (see softtabstop)
+  fileencoding   = 'utf-8',
+  filetype       = 'on',
+  fillchars      = vim.opt.fillchars + "diff:╱", -- Interface styling (see listchars)
+  fixeol         = true, -- Restore EOL at EOF if missing when writing
+  foldmethod     = "marker", -- Only allow foldings with triple brackets
+  formatoptions  = 'tcqrn1',
+  hidden         = true, -- Hide inactive buffers instead of deleting them
+  hlsearch       = true, -- Highlight all search matches
+  ignorecase     = true,
+  inccommand     = "split", -- Incrementally show effects of commands, opens split
+  incsearch      = true, -- Highlight search matches while writing (with hlsearch)
+  laststatus     = 2, -- Use a global statusline instead of one per window
+  linebreak      = true, -- Respect WORDS when wrap-breaking lines (see wrap)
+  modelines      = 0,
+  mouse          = "nvi", -- Allow mouse everywhere except in command line mode
+  nrformats      = "unsigned", -- Treat all numbers as unsigned with <C-A> and <C-X>
+  number         = true, -- Number column to the left (used with relativenumber)
+  pumblend       = 5,
   relativenumber = true, -- Show numbers relative to cursor position (see number)
-  scrolloff = 10, -- Leave 5 lines above and below cursor
-  shiftwidth = 2, -- Force indent spaces to equal to tabstop (see tabstop)
-  showcmd = true, -- Show the keys pressed in normal mode until action is run
-  cmdheight = 1,
-  showtabline = 2, -- Show the tabline even when just one tab is open
-  signcolumn = "yes", -- Always show the sign column beside the number (see number)
-  smartindent = true, -- Ident new lines in a smart way (see autoindent)
-  smarttab = true, -- Treat spaces as tabs in increments of shiftwidth
-  softtabstop = 2, -- Do not insert spaces when pressing tab (see shiftwidth)
-  splitbelow = true, -- Open splits below the current window
-  splitright = true, -- Open splits right of the current window
-  tabstop = 2, -- Number of columns to move when pressing <TAB> (see noexpandtab)
-  termguicolors = true, -- Enable 24-bit RGB color in the TUI
-  timeoutlen = 500, -- Milliseconds to wait before completing a mapped sequence
-  updatetime = 300, -- Milliseconds to wait before writing to swap file
+  scrolloff      = 10, -- Leave 5 lines above and below cursor
+  shiftround     = false,
+  shiftwidth     = 2, -- Force indent spaces to equal to tabstop (see tabstop)
+  showcmd        = true, -- Show the keys pressed in normal mode until action is run
+  showmode       = true,
+  showtabline    = 2, -- Show the tabline even when just one tab is open
+  signcolumn     = "yes", -- Always show the sign column beside the number (see number)
+  smartcase      = true,
+  smartindent    = true, -- Ident new lines in a smart way (see autoindent)
+  smarttab       = true, -- Treat spaces as tabs in increments of shiftwidth
+  softtabstop    = 2, -- Do not insert spaces when pressing tab (see shiftwidth)
+  splitbelow     = true, -- Open splits below the current window
+  splitright     = true, -- Open splits right of the current window
+  tabstop        = 2, -- Number of columns to move when pressing <TAB> (see noexpandtab)
+  termguicolors  = true, -- Enable 24-bit RGB color in the TUI
+  textwidth      = 79,
+  timeoutlen     = 500, -- Milliseconds to wait before completing a mapped sequence
+  title          = true,
+  ttyfast        = true,
+  updatetime     = 300, -- Milliseconds to wait before writing to swap file
   wildignorecase = true, -- Ignore case in filenames browsed by wildmenu
+  wildoptions    = "pum",
+  winblend       = 0,
+  wrap           = false, -- Do not wrap text that reaches the window's width
 }
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
 vim.wo.number = false
 vim.scriptencoding = 'utf-8'
@@ -77,10 +78,6 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 })
 
 vim.cmd [[ set statusline=%F%m%r%h%w\ [FORMAT=%{&ff}]\ [TYPE=%Y]\ [POS=%l,%v][%p%%]\ [BUFFER=%n]\ %{strftime('%c')} ]]
-
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
 
 vim.g.tex_flavor = "latex" -- Treat all .tex files as LaTeX instead of TeX
 vim.opt.shortmess:append("c")
