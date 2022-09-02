571c6247b20ed0b158a6a57d008200bfb245c3a1
guidomodarelli25 <guidomodarelli25@gmail.com>
2022-09-02 00:16:21 -0300

    move Telescope_buffer_dir

--- .config/nvim/after/plugin/telescope.rc.lua
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -1,11 +1,6 @@
 local status, telescope = pcall(require, 'telescope')
 if (not status) then return end
 local actions = require('telescope.actions')
-
-function telescope_buffer_dir()
-  vim.fn.expand("%:p:h")
-end
-
 local fb_actions = require 'telescope'.extensions.file_browser.actions
 
 telescope.setup {
--- .config/nvim/lua/maps/telescope.lua
+++ .config/nvim/lua/maps/telescope.lua
@@ -1,6 +1,10 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+function Telescope_buffer_dir()
+  vim.fn.expand("%:p:h")
+end
+
 keymap.set('n', ';f', '<cmd>lua require("telescope.builtin").find_files({ no_ignore = false, hidden = true })<CR>', opts)
 keymap.set('n', ';r', '<cmd>lua require("telescope.builtin").live_grep()<CR>', opts)
 keymap.set('n', ';b', '<cmd>lua require("telescope.builtin").buffers()<CR>', opts)
@@ -8,5 +12,5 @@ keymap.set('n', ';t', '<cmd>lua require("telescope.builtin").help_tags()<CR>', o
 keymap.set('n', ';;', '<cmd>lua require("telescope.builtin").resume()<CR>', opts)
 keymap.set('n', ';d', '<cmd>lua require("telescope.builtin").diagnostics()<CR>', opts)
 keymap.set('n', 'sf',
-  '<cmd>lua require("telescope").extensions.file_browser.file_browser({ path = "%:p:h", cwd = telescope_buffer_dir(), respect_git_ignore = false, hidden = true, grouped = true, previewer = false, initial_mode = "normal", layout_config = { height = 40 } })<CR>'
+  '<cmd>lua require("telescope").extensions.file_browser.file_browser({ path = "%:p:h", cwd = Telescope_buffer_dir(), respect_git_ignore = false, hidden = true, grouped = true, previewer = false, initial_mode = "normal", layout_config = { height = 40 } })<CR>'
   , opts)
