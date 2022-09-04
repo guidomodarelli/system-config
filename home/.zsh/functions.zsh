791a2276dd236bec3317b9151cba789512dd26d2
guidomodarelli25 <guidomodarelli25@gmail.com>
2022-09-04 17:26:08 -0300

    add function zsh and config tmux

---
+++ home/.zsh/functions.zsh
@@ -0,0 +1,4 @@
+fif() {
+  if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+  rg --hidden --files-with-matches --no-messages "$1" | fzf --preview "rg --ignore-case --pretty --context 10 '$1' {}"
+}
