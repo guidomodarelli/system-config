df5246db8e622c43dd1f0b03377786804f51a120
guidomodarelli25 <guidomodarelli25@gmail.com>
2022-09-21 01:19:59 -0300

    delete empty lines

--- files/ZSHRC/.zsh/mappings.zsh
+++ files/ZSHRC/.zsh/mappings.zsh
@@ -3,13 +3,9 @@ bindkey -e
 
 if command -v fzf &>/dev/null; then
 	bindkey '^b' anyframe-widget-checkout-git-branch
-
 	bindkey '^r' anyframe-widget-execute-history
-
 	bindkey '^g' anyframe-widget-cd-ghq-repository
-
 	bindkey '^k' anyframe-widget-kill
-
 	zstyle ':anyframe:selector:' use fzf
 fi
 
