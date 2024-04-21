8e548e6d94c2d2621a700fe87132cb208eb6ae73
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-04-21 14:54:33 -0300

    fix: add ExecutionPolicy Bypass

--- dotfiler.bat
+++ dotfiler.bat
@@ -1,1 +1,1 @@
-powershell -Command "New-Item -ItemType SymbolicLink -Path C:\Users\Guido\AppData\Roaming\espanso\ -Target C:\Users\Guido\system-config\files\.config\espanso\ "
+powershell -ExecutionPolicy Bypass -Command "New-Item -ItemType SymbolicLink -Path C:\Users\Guido\AppData\Roaming\espanso\ -Target C:\Users\Guido\system-config\files\.config\espanso\ "
