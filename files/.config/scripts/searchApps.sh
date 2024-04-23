7bdf5c3d351111e0d3c49739341d8072243ac610
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-04-23 02:34:27 -0300

    chore: Rename scripts to rofi directory

--- files/.config/scripts/searchApps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-
-rofi \
-	-show drun \
-	-disable-history \
-	-config ~/.config/rofi/rofidmenu.rasi
--- files/.config/scripts/searchEmojis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-
-rofi \
-	-modi emoji \
-	-show emoji \
-	-config ~/.config/rofi/rofidmenu.rasi
