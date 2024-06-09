dc12883fabc2b000afa1f6c29d7eb5362c15ad77
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-09 14:07:24 -0300

    Add 'rofi' to the list of installed packages

--- setup.sh
+++ setup.sh
@@ -73,7 +73,7 @@ install_font_Iosevka() {
 
 main() {
 	# Install dependendencies
-	sudo pacman -Sy --noconfirm base-devel gcc git-delta neovim fzf ripgrep fd bat kubectl curlie exa zoxide ranger wezterm
+	sudo pacman -Sy --noconfirm base-devel gcc git-delta neovim fzf ripgrep fd bat kubectl curlie exa zoxide ranger wezterm rofi
 
 	install_vscode
 	install_homebrew
