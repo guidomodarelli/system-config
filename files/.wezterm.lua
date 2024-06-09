cd5b28b6293d541fcacafbfe7647ba0147672cc1
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-09 14:27:04 -0300

    Update font name in Wezterm config to "IosevkaTerm NF".

--- files/.wezterm.lua
+++ files/.wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback({
-	"Iosevka Term Curly",
+	"IosevkaTerm NF",
 	"JetBrains Mono",
 })
 
