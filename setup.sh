4d4634626ad41edad2ecffc5ce11e3bdfb6da2a8
guidomodarelli25 <guidomodarelli25@gmail.com>
2024-06-19 14:41:27 -0300

    Update setup.sh to include jq as a system dependency

--- setup.sh
+++ setup.sh
@@ -127,7 +127,7 @@ install_homebrew_dependencies() {
 
 install_dependencies() {
 	# System dependencies
-	sudo pacman -Sy --noconfirm yay base-devel gcc vlc peek telegram-desktop git-delta neovim fzf ripgrep fd bat kubectl curlie exa zoxide ranger wezterm rofi git-filter-repo btop ttf-iosevkaterm-nerd ttf-jetbrains-mono ttf-victor-mono-nerd ttf-dejavu-nerd ttf-cascadia-mono-nerd awesome
+	sudo pacman -Sy --noconfirm yay base-devel gcc jq vlc peek telegram-desktop git-delta neovim fzf ripgrep fd bat kubectl curlie exa zoxide ranger wezterm rofi git-filter-repo btop ttf-iosevkaterm-nerd ttf-jetbrains-mono ttf-victor-mono-nerd ttf-dejavu-nerd ttf-cascadia-mono-nerd awesome
 	yay -S --noconfirm --needed lazygit-git lyx
 
 	# Go dependencies
