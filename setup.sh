c41c1a887e0ad9cc2067d31d512dd5c850254938
Guido Modarelli <guidomodarelli25@gmail.com>
2024-09-15 16:04:40 -0300

    Update setup.sh with new package dependencies

--- setup.sh
+++ setup.sh
@@ -127,8 +127,8 @@ install_homebrew_dependencies() {
 
 install_dependencies() {
 	# System dependencies
-	sudo pacman -Sy --noconfirm yay base-devel gcc jq vlc peek telegram-desktop git-delta neovim fzf ripgrep fd bat kubectl curlie exa zoxide ranger wezterm rofi git-filter-repo btop ttf-iosevkaterm-nerd ttf-jetbrains-mono ttf-victor-mono-nerd ttf-dejavu-nerd ttf-cascadia-mono-nerd awesome
-	yay -S --noconfirm --needed lazygit-git lyx
+	sudo pacman -Sy --noconfirm yay base-devel gcc jq vlc peek telegram-desktop git-delta neovim fzf ripgrep fd bat curlie exa zoxide ranger wezterm rofi git-filter-repo btop ttf-iosevkaterm-nerd ttf-jetbrains-mono ttf-victor-mono-nerd ttf-dejavu-nerd ttf-cascadia-mono-nerd xclip
+	yay -S --noconfirm --needed lazygit-git difft
 
 	# Go dependencies
 	install_golang
