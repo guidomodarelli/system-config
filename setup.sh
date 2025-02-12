4624cc72fb0ff54bf71f9b74b6dfa482b75c7438
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-12 20:17:34 +0000

    feat: implement exa installation in setup script for Ubuntu

--- setup.sh
+++ setup.sh
@@ -162,7 +162,10 @@ install_user_interface_apps() {
 
 install_exa() {
 	if is_ubuntu; then
-		# TODO: install exa
+		EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+		curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
+		sudo unzip -q exa.zip bin/exa -d /usr/local
+		rm -rf exa.zip
 	else
 		sudo pacman -Sy --noconfirm exa
 	fi
