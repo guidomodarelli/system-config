613a57ca4647ba27d15332f25373e0e7b717718e
Guido <guidomodarelli25@gmail.com>
2025-02-12 19:45:16 +0000

    feat: create local bin directory and symlink for fdfind in installation script

--- setup.sh
+++ setup.sh
@@ -177,6 +177,7 @@ install_homebrew() {
 install_fd_find() {
 	if is_ubuntu; then
 		sudo apt install -y fd-find
+		mkdir -p ~/.local/bin
 		ln -s $(which fdfind) ~/.local/bin/fd
 	else
 		sudo pacman -Sy --noconfirm fd
