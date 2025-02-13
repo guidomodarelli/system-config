eb179e19a550ad933e4df703d75cd69246d8bad4
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-13 14:11:17 +0000

    feat: add PowerShell setup script for Chocolatey and font installation

---
+++ setup.ps1
@@ -0,0 +1,18 @@
+# NOTE: Run this script as Administrator
+
+function Install-Choco {
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+function Install-Fonts {
+	choco install jetbrainsmono --confirm --no-progress
+}
+
+function Main {
+	Install-Choco
+	Install-Fonts
+}
+
+Main
