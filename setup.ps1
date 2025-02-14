5b64fafce328edfa9709b98a8a06a3c8fdecc78e
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-14 19:39:55 -0300

    feat: update espanso service registration in PowerShell setup script

--- setup.ps1
+++ setup.ps1
@@ -33,7 +33,8 @@ function Install-Espanso {
 
 	Remove-Item -Recurse -Force C:\Users\Guido\AppData\Roaming\espanso\
 	New-Item -ItemType SymbolicLink -Path C:\Users\Guido\AppData\Roaming\espanso\ -Target C:\Users\Guido\system-config\files\.config\espanso\
-	espanso restart
+
+	espanso service register
 }
 
 function Main {
