c705ce3ef3ef698649862ac829643a65c504d967
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-15 15:00:47 -0300

    feat: replace standard output message with informational message for Chocolatey installation check

--- setup.ps1
+++ setup.ps1
@@ -19,7 +19,7 @@ function Install-Choco {
 		[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 		iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 	} else {
-		Write-Host "Chocolatey is already installed."
+		Write-InfoMessage "Chocolatey is already installed."
 	}
 }
 
