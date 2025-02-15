f4a5e0208801aa643d41c6dbdb23b3cc62ea40e5
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-15 21:52:23 +0000

    refactor: update setup script directory handling for Windows and Linux

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ import subprocess
 from pathlib import Path
 import sys
 
-def setup_windows(script_dir: Path, args: list[str]):
-	setup_script_path = script_dir / "setup.ps1"
+def setup_windows(setup_dir: Path, args: list[str]):
+	setup_script_path = setup_dir / "setup.ps1"
 	subprocess.call(["powershell", "-ExecutionPolicy", "Bypass", "-File", str(setup_script_path.absolute())] + args)
 
-def setup_linux(script_dir: Path, args: list[str]):
-	setup_script_path = script_dir / "setup.sh"
+def setup_linux(setup_dir: Path, args: list[str]):
+	setup_script_path = setup_dir / "setup.sh"
 	subprocess.call([str(setup_script_path.absolute())] + args)
 
 	dotfiler_path = Path("dotfiler.sh")
@@ -18,14 +18,16 @@ def setup_linux(script_dir: Path, args: list[str]):
 
 def main(args: list[str]):
 	try:
-		script_dir = Path("scripts/setup")
+		script_dir = Path("scripts")
+		setup_dir = script_dir / "setup"
 		system = platform.system()
 		if system == "Windows":
-			setup_windows(script_dir, args)
+			setup_windows(setup_dir, args)
 		elif system == "Linux":
-			setup_linux(script_dir, args)
+			setup_linux(setup_dir, args)
 		else:
 			print(f"Unsupported operating system: {system}")
+		print("Setup complete.")
 	except KeyboardInterrupt:
 		print("\nProcess interrupted by user. Exiting gracefully.")
 	except Exception as e:
