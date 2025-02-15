b43367e790b732f1393e0260bb065892672c1146
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-15 17:22:15 -0300

    feat: add PowerShell setup script and enhance Python installation process

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import platform
 import subprocess
+import pathlib
 
 def main():
 	try:
 		system = platform.system()
 		if system == "Windows":
-			subprocess.call(["setup.bat"])
+			script_path = pathlib.Path("scripts/setup/setup.ps1")
+			subprocess.call(["powershell", "-ExecutionPolicy", "Bypass", "-Command", str(script_path.absolute())])
 		elif system == "Linux":
-			subprocess.call(["bash", "setup.sh"])
+			script_path = pathlib.Path("scripts/setup/setup.sh")
+			subprocess.call([str(script_path.absolute())])
 		else:
 			print(f"Unsupported operating system: {system}")
 	except KeyboardInterrupt:
 		print("\nProcess interrupted by user. Exiting gracefully.")
+	except Exception as e:
+		print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
 	main()
