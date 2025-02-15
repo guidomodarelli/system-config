ca8cbd47f3e2dbd997590e07147fc57e12bb491e
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-15 01:21:18 -0300

    feat: start espanso service after installation

--- setup.ps1
+++ setup.ps1
@@ -37,6 +37,7 @@ function Install-Espanso {
 	New-Item -ItemType SymbolicLink -Path C:\Users\Guido\AppData\Roaming\espanso\ -Target C:\Users\Guido\system-config\files\.config\espanso\
 
 	espanso service register
+	espanso start
 }
 
 function Install-LazyVim {
