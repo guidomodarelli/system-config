a3139030c95ef00135d0b82dd9362c7ceb45517a
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-17 15:42:39 +0000

    fix: prevent Vagrant installation on Windows

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -353,6 +353,8 @@ install_utilities() {
 }
 
 install_vagrant() {
+	is_windows && return
+
 	wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 	sudo apt update && sudo apt install -y vagrant
