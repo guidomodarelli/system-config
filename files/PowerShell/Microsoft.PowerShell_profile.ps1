3b0f49b6cff7c8dfe02e563c436d2b40757c99f9
Guido Modarelli <guidomodarelli25@gmail.com>
2025-03-18 17:59:06 +0000

    Adds FZF configuration for PowerShell and updates zsh submodule
    Configures FZF default options, header, prompt, and colors in PowerShell profile
    Updates zsh submodule to the latest commit

--- files/PowerShell/Microsoft.PowerShell_profile.ps1
+++ files/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -292,3 +292,17 @@ function lt {
     & eza @(($lt_options -split ' ') + $args)
   }
 }
+
+# FZF configuration
+$FZF_DEFAULT_HEADER_WITH_MULTI = '(Multi-select) Select items with TAB and ENTER to confirm'
+$FZF_DEFAULT_HEADER_WITH_SINGLE = '(Single-select) Select item with ENTER to confirm'
+$FZF_PREFIX_PROMPT = '🔍 '
+$FZF_DEFAULT_BIND = 'ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all'
+$FZF_COLOR_MOLOKAI = 'bg+:#293739,bg:#1B1D1E,border:#808080,spinner:#E6DB74,hl:#7E8E91,fg:#F8F8F2,header:#7E8E91,info:#A6E22E,pointer:#A6E22E,marker:#F92672,fg+:#F8F8F2,prompt:#F92672,hl+:#F92672'
+
+# Set FZF pointer and marker symbols (these need to be defined)
+$POINTER = '➤'
+$MARKER = '✓'
+
+# Set FZF default options
+$env:FZF_DEFAULT_OPTS = "--color=`"$FZF_COLOR_MOLOKAI`" --ansi --cycle --border=rounded --prompt=`"$FZF_PREFIX_PROMPT`" --pointer=$POINTER --marker=$MARKER --header=`"$FZF_DEFAULT_HEADER_WITH_SINGLE`" --multi=0 --bind=`"$FZF_DEFAULT_BIND`""
