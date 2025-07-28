bb2ea9546c96d9c42d247a190d50b5fed1c00a2f
Guido Modarelli <guidomodarelli25@gmail.com>
2025-07-28 22:28:21 +0000

    Adds fix broken install for Ubuntu
    Ensures that broken packages are fixed before installing essential packages on Ubuntu systems.
    
    This addresses potential issues during the installation process by resolving any dependency conflicts or incomplete installations.

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -345,6 +345,7 @@ install_zsh() {
 
 install_essencials() {
   if is_ubuntu; then
+    sudo apt --fix-broken install
     sudo apt install -y build-essential gcc curl wget zip unzip python3.12-venv
   elif is_arch; then
     sudo pacman -Sy --noconfirm yay base-devel gcc curl wget zip unzip
