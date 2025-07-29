23938c2b219453da26ddfd192d0086b6f158a25f
Guido Modarelli <guidomodarelli25@gmail.com>
2025-07-29 15:32:37 +0000

    Uses apt-get for docker installation
    Ensures compatibility with older Ubuntu versions by
    using `apt-get` instead of `apt` for installing Docker.

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -57,8 +57,8 @@ install_oh_my_zsh() {
 install_docker() {
   if is_ubuntu; then
     # Add Docker's official GPG key:
-    sudo apt update
-    sudo apt install ca-certificates curl
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -68,8 +68,8 @@ install_docker() {
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
       $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update
-    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   elif is_arch; then
     sudo pacman -Sy --noconfirm docker docker-compose
   fi
