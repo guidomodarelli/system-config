3eb6ada40d2b7bae672a746a7da84db1ca938676
Guido Modarelli <guidomodarelli25@gmail.com>
2025-07-29 15:28:12 +0000

    Installs Docker using the official repository
    Updates the Docker installation process on Ubuntu to use the official Docker repository.
    This ensures that the latest version of Docker is installed and simplifies future updates.
    It also adds Docker Compose plugin.

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -56,11 +56,24 @@ install_oh_my_zsh() {
 
 install_docker() {
   if is_ubuntu; then
+    # Add Docker's official GPG key:
     sudo apt update
-    sudo apt install -y docker.io docker-compose
+    sudo apt install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   elif is_arch; then
     sudo pacman -Sy --noconfirm docker docker-compose
   fi
+  sleep 3
   sudo systemctl start docker.service
   sudo systemctl enable docker.service
   sudo usermod -aG docker $USER
