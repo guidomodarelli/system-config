89846f7c2855641521b4564a9890acf020bedf59
Guido Modarelli <guidomodarelli25@gmail.com>
2025-08-28 16:42:08 +0000

    add function «DDstart» for executing Docker containers interactively
    Introduce a new function «DDstart» to streamline the process of
    executing Docker containers interactively. The function leverages
    «fzf» for container selection and constructs the appropriate
    «docker exec» commands, improving usability and efficiency in
    managing containers.

--- configs/zsh/.zsh/functions/docker.zsh
+++ configs/zsh/.zsh/functions/docker.zsh
@@ -125,6 +125,14 @@ DDexec() {
   done | anyframe-action-put
 }
 
+DDstart() {
+  logInfo "Executing Docker containers..."
+  docker_table_formatter | fzf --prompt="$FZF_PREFIX_PROMPT docker exec " | awk '{print $2}' | while IFS= read -r sel; do
+    { styleText -c yellow -b -i "docker exec -it "; styleText -c cyan -b "$sel"; echo } >&2
+    echo "docker exec -it $sel yarn start --no-base-path"
+  done | anyframe-action-put
+}
+
 DDlogs() {
   docker_table_formatter | fzf --prompt="$FZF_PREFIX_PROMPT docker logs " | awk '{print $2}' | while IFS= read -r sel; do
     { styleText -c yellow -b -i "docker logs "; styleText -c cyan -b "$sel"; echo } >&2
