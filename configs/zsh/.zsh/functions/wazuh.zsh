5e122c124657f87364f6642eca936857be79ca62
Guido Modarelli <guidomodarelli25@gmail.com>
2025-08-29 12:45:00 +0000

    rename function «WZstart» to «wzstart» and improve terminal output
    Rename function «WZstart» to follow consistent naming conventions.
    Enhance user experience by adding a «clear» command to the terminal
    output before executing Docker containers.

--- configs/zsh/.zsh/functions/wazuh.zsh
+++ configs/zsh/.zsh/functions/wazuh.zsh
@@ -1,6 +1,6 @@
-WZstart() {
+wzstart() {
   logInfo "Executing Docker containers..."
   docker_table_formatter | fzf --prompt="$FZF_PREFIX_PROMPT docker exec " | awk '{print $2}' | while IFS= read -r sel; do
-    echo "docker exec -it $sel yarn start --no-base-path"
+    echo "clear; docker exec -it $sel yarn start --no-base-path"
   done | anyframe-action-put
 }
