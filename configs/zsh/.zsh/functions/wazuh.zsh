71dc2036531b92e5f39ac0b00c5cb2fe33654d01
Guido Modarelli <guidomodarelli25@gmail.com>
2025-09-10 20:09:40 +0000

    filter Docker containers by name in «wzstart»
    restrict selection in «wzstart» to containers matching 'osd-dev'
    or 'dashboard' and exclude those containing 'runner' to improve
    usability and reduce irrelevant choices in the interactive prompt

--- configs/zsh/.zsh/functions/wazuh.zsh
+++ configs/zsh/.zsh/functions/wazuh.zsh
@@ -1,6 +1,6 @@
 wzstart() {
   logInfo "Executing Docker containers..."
-  docker_table_formatter | fzf --prompt="$FZF_PREFIX_PROMPT docker exec '" | awk '{print $2}' | while IFS= read -r sel; do
+  docker_table_formatter | grep -v "runner" | grep -E "(osd-dev|dashboard)" | fzf --prompt="$FZF_PREFIX_PROMPT docker exec " --query "'" | awk '{print $2}' | while IFS= read -r sel; do
     echo "clear; docker exec -it $sel yarn start --no-base-path"
   done | anyframe-action-put
 }
