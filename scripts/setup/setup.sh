0fb17baf7f70c3f5dfd56fc52c710886816e6dd3
Guido Modarelli <guidomodarelli25@gmail.com>
2025-10-15 12:34:50 -0300

    replace python3.12-venv with python3-venv for compatibility
    Ensures broader compatibility on Ubuntu by installing «python3-venv» instead of the version-specific package, reducing potential issues with future Python updates.

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -356,7 +356,7 @@ install_zsh() {
 
 install_essencials() {
   if is_ubuntu; then
-    sudo apt install -y build-essential gcc curl wget zip unzip python3.12-venv
+    sudo apt install -y build-essential gcc curl wget zip unzip python3-venv
   elif is_arch; then
     sudo pacman -Sy --noconfirm yay base-devel gcc curl wget zip unzip
   fi
