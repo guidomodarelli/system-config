04055e5cdab9a1e99540b1c33d9c7fa3be9b6464
Guido Modarelli <guidomodarelli25@gmail.com>
2025-10-31 02:22:07 +0000

    remove unnecessary dependency comment
    The comment regarding assumed dependencies has been removed to streamline the code and improve clarity. This change does not affect functionality but enhances the readability of the documentation within the code.

--- configs/zsh/.zsh/functions/selector-utils.zsh
+++ configs/zsh/.zsh/functions/selector-utils.zsh
@@ -1,5 +1,4 @@
 # Generic selector utilities (shared by git/docker/wazuh selectors)
-# Dependencias asumidas: logCyan, logGray, styleText, fzf, column
 # API:
 # selector_list <descriptions_fn>
 #   Llama a la función <descriptions_fn> que debe emitir lineas 'func|descripcion'
