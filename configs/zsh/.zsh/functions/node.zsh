a9859b1aa31131af790ac4a34b7dac53e870d13c
Guido Modarelli <guidomodarelli25@gmail.com>
2025-11-03 19:50:52 +0000

    update function «@prettier:fix» to streamline directory navigation
    Replaced the use of «pushd» with «cd» for changing directories to simplify the function's flow. This change improves readability and maintains the same functionality without the need for returning to the previous directory. Additionally, cleaned up the formatting of the code for better consistency.

--- configs/zsh/.zsh/functions/node.zsh
+++ configs/zsh/.zsh/functions/node.zsh
@@ -4,13 +4,12 @@
     return 1
   fi
 
-  pushd $(git rev-parse --show-toplevel)
+  cd $(git rev-parse --show-toplevel)
 
-    BIN_PRETTIER=$(find . -type f,l -name "prettier" | sort | grep 'node_modules/.bin/prettier' | head -n 1)
+  BIN_PRETTIER=$(find . -type f,l -name "prettier" | sort | grep 'node_modules/.bin/prettier' | head -n 1)
 
-    BRANCH=$(git branch | grep -v $(git branch --show-current) | sed -E 's!\\*?[ ]+!!g' | grep -vE '[0-9]+-' | fzf --prompt='Select branch to format against: ')
+  BRANCH=$(git branch | grep -v $(git branch --show-current) | sed -E 's!\\*?[ ]+!!g' | grep -vE '[0-9]+-' | fzf --prompt='Select branch to format against: ')
 
-    git diff --name-status $(git merge-base HEAD $BRANCH) HEAD | grep -vE "^D" | awk '{ print $2 }' | xargs -I{} $BIN_PRETTIER --write {} --config .prettierrc
-
-  popd
+  git diff --name-status $(git merge-base HEAD $BRANCH) HEAD | grep -vE "^(D|R)" | awk '{ print $2 }' | xargs -I{} $BIN_PRETTIER --write {} --config .prettierrc
+  git diff --name-status $(git merge-base HEAD $BRANCH) HEAD | grep -E "^R" | awk '{ print $3 }' | xargs -I{} $BIN_PRETTIER --write {} --config .prettierrc
 }
