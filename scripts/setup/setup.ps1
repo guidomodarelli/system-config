feb5857c130e20bac9596116fa4f12e509676242
Guido Modarelli <guidomodarelli25@gmail.com>
2025-12-22 16:24:54 -0300

    refactor logging functions in setup script
    Replace the existing logging functions with a consistent naming convention. The functions «Write-ErrorMessage», «Write-InfoMessage», «Write-WarningMessage», and «Write-SuccessMessage» have been renamed to «LogError», «LogInfo», «LogWarning», and «LogSuccess» respectively. This change improves code readability and maintains uniformity across the logging functionality. All instances of the old function names have been updated accordingly.

--- scripts/setup/setup.ps1
+++ scripts/setup/setup.ps1
@@ -1,27 +1,27 @@
 # NOTE: Run this script as Administrator
 
-function Write-ErrorMessage {
+function LogError {
   param (
     [string]$message
   )
   Write-Host "[ ERROR ] $message" -ForegroundColor Red
 }
 
-function Write-InfoMessage {
+function LogInfo {
   param (
     [string]$message
   )
   Write-Host "[ INFO ] $message" -ForegroundColor Blue
 }
 
-function Write-WarningMessage {
+function LogWarning {
   param (
     [string]$message
   )
   Write-Host "[ WARNING ] $message" -ForegroundColor Yellow
 }
 
-function Write-SuccessMessage {
+function LogSuccess {
   param (
     [string]$message
   )
@@ -33,9 +33,9 @@ function Install-Choco {
   if (-Not (Test-Path 'C:\ProgramData\chocolatey\bin\choco.exe')) {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    Write-SuccessMessage "Chocolatey has been installed successfully."
+    LogSuccess "Chocolatey has been installed successfully."
   } else {
-    Write-WarningMessage "Chocolatey is already installed."
+    LogWarning "Chocolatey is already installed."
   }
 }
 
@@ -43,9 +43,9 @@ function Install-Scoop {
   if (-Not (Test-Path "$env:USERPROFILE\scoop\shims\scoop.ps1")) {
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
     Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
-    Write-SuccessMessage "Scoop has been installed successfully."
+    LogSuccess "Scoop has been installed successfully."
   } else {
-    Write-WarningMessage "Scoop is already installed."
+    LogWarning "Scoop is already installed."
   }
 }
 
@@ -55,13 +55,13 @@ function Install-ChocoPackages {
   )
   foreach ($package in $packages) {
     if (choco list | Select-String -Pattern $package) {
-      Write-WarningMessage "The package '$package' is already installed. Upgrading..."
+      LogWarning "The package '$package' is already installed. Upgrading..."
       choco upgrade $package --confirm --no-progress
-      Write-SuccessMessage "The package '$package' has been upgraded successfully."
+      LogSuccess "The package '$package' has been upgraded successfully."
     } else {
-      Write-InfoMessage "Installing the package '$package'..."
+      LogInfo "Installing the package '$package'..."
       choco install $package --confirm --no-progress
-      Write-SuccessMessage "The package '$package' has been installed successfully."
+      LogSuccess "The package '$package' has been installed successfully."
     }
   }
 }
@@ -72,9 +72,9 @@ function Install-WingetPackages {
   )
   foreach ($appId in $appIds) {
     $appName = (winget search -e --id $appId | Select-Object -Last 1 | ForEach-Object { $_.Split(" ")[0] })
-    Write-InfoMessage "Installing the package '$appName'..."
+    LogInfo "Installing the package '$appName'..."
     winget install -e --id $appId
-    Write-SuccessMessage "The package '$appName' has been installed successfully."
+    LogSuccess "The package '$appName' has been installed successfully."
   }
 }
 
@@ -154,13 +154,13 @@ function Install-Eza {
 }
 
 function Install-WSL {
-  Write-InfoMessage "Checking if WSL is installed..."
+  LogInfo "Checking if WSL is installed..."
   if (wsl --list --quiet) {
-    Write-WarningMessage "WSL is already installed."
+    LogWarning "WSL is already installed."
   } else {
-    Write-InfoMessage "Installing WSL..."
+    LogInfo "Installing WSL..."
     wsl --install
-    Write-SuccessMessage "WSL has been installed successfully."
+    LogSuccess "WSL has been installed successfully."
   }
 }
 
@@ -186,25 +186,25 @@ function Test-HyperVAvailability {
 }
 
 function Enable-HyperV {
-  Write-InfoMessage "Enabling Hyper-V..."
+  LogInfo "Enabling Hyper-V..."
   if (-not (Test-HyperVAvailability)) {
-    Write-WarningMessage "Hyper-V is not available on this Windows edition."
+    LogWarning "Hyper-V is not available on this Windows edition."
     return
   }
   try {
     $feature = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -ErrorAction Stop
     if ($feature.State -eq 'Enabled') {
-      Write-WarningMessage "Hyper-V is already enabled."
+      LogWarning "Hyper-V is already enabled."
       return
     }
     $result = Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart -ErrorAction Stop
     if ($result.RestartNeeded) {
-      Write-SuccessMessage "Hyper-V has been enabled successfully. A restart is required to complete the installation."
+      LogSuccess "Hyper-V has been enabled successfully. A restart is required to complete the installation."
     } else {
-      Write-SuccessMessage "Hyper-V has been enabled successfully."
+      LogSuccess "Hyper-V has been enabled successfully."
     }
   } catch {
-    Write-ErrorMessage ("Failed to enable Hyper-V. {0}" -f $_.Exception.Message)
+    LogError ("Failed to enable Hyper-V. {0}" -f $_.Exception.Message)
   }
 }
 
@@ -287,19 +287,19 @@ function Main {
 }
 
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-  Write-ErrorMessage "Please run this script as Administrator."
+  LogError "Please run this script as Administrator."
   exit
 }
 
 if ($args.Count -gt 0) {
   $functionName = $args[0]
   if (Get-Command -Name $functionName -CommandType Function -ErrorAction SilentlyContinue) {
-    Write-InfoMessage "Invoking function: $functionName"
+    LogInfo "Invoking function: $functionName"
     Invoke-Expression $functionName
   } else {
-    Write-InfoMessage "Function "$functionName" does not exist."
+    LogInfo "Function "$functionName" does not exist."
   }
 } else {
-  Write-InfoMessage "No arguments were passed. Running default functions."
+  LogInfo "No arguments were passed. Running default functions."
   Main
 }
