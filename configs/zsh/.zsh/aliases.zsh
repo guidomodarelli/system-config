bded5a8597ab793892619b0bf47637e5640bb594
Guido Modarelli <guidomodarelli25@gmail.com>
2026-03-27 09:14:25 -0300

    update alias for «rg» command and replace «exa» with «eza»
    This commit modifies the alias for the «rg» command to include the «-p» flag for better performance. Additionally, it replaces references to the «exa» command with the «eza» command, ensuring that the aliases are consistent with the current toolset being used. Unused aliases for «kubectl» and «curlie» have been removed to streamline the configuration.

--- configs/zsh/.zsh/aliases.zsh
+++ configs/zsh/.zsh/aliases.zsh
@@ -1,5 +1,5 @@
 ### General
-alias rg="rg --glob \"!.git/*\""
+alias rg="rg -p --glob \"!.git/*\""
 alias so=source
 alias gt=grt
 
@@ -11,10 +11,6 @@ if command -v fzf >/dev/null 2>&1; then
 	alias f=fzf
 fi
 
-if command -v kubectl >/dev/null 2>&1; then
-	alias k=kubectl
-fi
-
 if command -v batcat >/dev/null 2>&1; then
 	alias bat=batcat
 fi
@@ -23,15 +19,10 @@ if command -v python3 >/dev/null 2>&1; then
 	alias py=python3
 fi
 
-if command -v curlie >/dev/null 2>&1; then
-	alias curl=curlie
-fi
-
-### exa
-if command -v exa >/dev/null 2>&1; then
-	alias exa='exa --group-directories-first --icons'
-	alias ll='exa --long --header --group'
+### eza
+if command -v eza >/dev/null 2>&1; then
+	alias eza='eza --group-directories-first --icons'
+	alias ll='eza --long --header --group'
 	alias la='ll --all'
-	alias lt='exa --tree --all'
+	alias lt='eza --tree --all'
 fi
-
