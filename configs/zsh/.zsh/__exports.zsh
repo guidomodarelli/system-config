0c431a67d4cd010e019a318201498f494a9e4498
Guido Modarelli <guidomodarelli25@gmail.com>
2026-08-06 14:47:47 -0300

    update: add Visual Studio Code to PATH for macOS users

--- configs/zsh/.zsh/__exports.zsh
+++ configs/zsh/.zsh/__exports.zsh
@@ -51,6 +51,11 @@ if [[ "$CURRENT_OS" = "Darwin" ]]; then
   if [[ -n "${HOMEBREW_PREFIX:-}" ]]; then
     export PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:$PATH"
   fi
+
+  ### Visual Studio Code
+  if [[ -x "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" ]]; then
+    path+=("/Applications/Visual Studio Code.app/Contents/Resources/app/bin")
+  fi
 fi
 
 # =============================================================================
